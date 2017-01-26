d082abce73224495c3a6626a69e1bea168d9a133
markdolecki <markdolecki@users.noreply.github.com>
2017-01-25 21:44:09 -0800

    Initial commit of modularized LiftMechanism and LinearSlide components.

---
+++ src/main/java/org/firstinspires/ftc/mentor/LiftMechanism.java
@@ -0,0 +1,313 @@
+package org.firstinspires.ftc.mentor;
+
+import com.qualcomm.ftccommon.DbgLog;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import java.util.Map;
+
+
+/*
+ * Class describing a lift mechanism that us used to raise a load to a specific height.
+ *
+ * Note: Assumes:
+ * 1. Lift has two motors of the same motor type.
+ * 2. Both motors have encoders.
+ */
+class LiftMechanism {
+
+    private boolean DEBUG = false;
+    private LinearOpMode opMode;
+    private double TARGET_HEIGHT_IN = 12.0; // Target lift height
+    private DcMotor motor1;
+    private DcMotor motor2;
+    private Map<DcMotor, MotorMetadata> motorMetadata;
+//    private MotorMetadata.MotorType motorType = MotorMetadata.MotorType.ANDYMARK_NEVEREST_40; // default to AndyMark Neverest 40
+    private double LIFT_GEAR_REDUCTION = 1.0;
+    private double DEFAULT_LIFT_SPEED = 0.5;
+    private double SPOOL_DIAMETER = 2.5; // inches
+    private double STRING_LENGTH = 14.0; // inches
+    private double currHeight = 0;
+    private boolean liftStuck = false;
+
+    enum LiftState {
+        RETRACTED,
+        RAISING,
+        RAISED,
+        LOWERING,
+        UNKNOWN
+    }
+
+    private LiftState liftState = LiftState.RETRACTED; // Default assumes in starting state
+
+    private LiftMechanism() {
+        // Default constructor
+    }
+
+    public LiftMechanism(LinearOpMode opMode, DcMotor motor1, DcMotor motor2, Map<DcMotor, MotorMetadata> metadata, double height, boolean debug) {
+        if (opMode == null) {
+            opModeRequired();
+        }
+        this.opMode = opMode;
+
+        this.motor1 = motor1;
+        this.motor2 = motor2;
+        this.motorMetadata = metadata;
+        this.TARGET_HEIGHT_IN = height;
+        this.DEBUG = debug;
+    }
+
+    public LiftMechanism(LinearOpMode opMode, DcMotor motor1, DcMotor motor2, Map<DcMotor, MotorMetadata> metadata, double height) {
+        if (opMode == null) {
+            opModeRequired();
+        }
+        this.opMode = opMode;
+
+        this.motor1 = motor1;
+        this.motor2 = motor2;
+        this.motorMetadata = metadata;
+        this.TARGET_HEIGHT_IN = height;
+    }
+
+    public LiftMechanism(LinearOpMode opMode, DcMotor motor1, DcMotor motor2, Map<DcMotor, MotorMetadata> metadata) {
+        if (opMode == null) {
+            opModeRequired();
+        }
+        this.opMode = opMode;
+
+        this.motor1 = motor1;
+        this.motor2 = motor2;
+        this.motorMetadata = metadata;
+    }
+
+//    public LiftMechanism(LinearOpMode opMode, DcMotor motor1, DcMotor motor2) {
+//        if (opMode == null) {
+//            opModeRequired();
+//        }
+//        this.opMode = opMode;
+//
+//        this.motor1 = motor1;
+//        this.motor2 = motor2;
+//    }
+
+    LiftState getLiftState() {
+        String functionName = "getLiftState";
+
+        if (DEBUG) {
+            DbgLog.msg("%s", functionName);
+        }
+        return this.liftState;
+    }
+
+    LiftState detectLiftState() {
+        String functionName = "detectLiftState";
+
+        if (DEBUG) {
+            DbgLog.msg("%s", functionName);
+        }
+
+        // TODO: Detect the state of the lift if possible
+        // Ideally there is a way to use a sensor to detect which state the lift is in
+        //
+        unsupported(functionName);
+
+        // Check a sensor to determine if lift is in its retracted state.
+        // Options include a Hall sensor, IR Beam Break sensor, etc.
+//        if (SOMESENSOR == SOMEVALUE) { // add the appropriate logic here.
+//            return LiftState.RETRACTED;
+//        }
+
+        // If the motors are busy, then lift is either raising or lowering.
+        if (motor1.isBusy() || motor2.isBusy()) {
+            // Check motor power level for motor1
+            // If motor one power level is < 0 / > 0, ????, then lift is extending
+            // TODO: What is appropriate test here?
+
+
+            double power = motor1.getPower();
+            if (power > 0) {
+                return LiftState.RAISING;
+            }
+            else {
+                return LiftState.LOWERING;
+            }
+        }
+
+        // If not retracted and motors are not moving, then lift is raised.
+//        return LiftState.RAISED;
+
+
+        // Assume retracted until sensor logic is added.
+        return LiftState.RETRACTED;
+    }
+
+    // Set Lift motor power
+    void setLiftSpeed(double speed) {
+        motor1.setPower(speed);
+        motor2.setPower(speed);
+    }
+
+    void stopLift() {
+        motor1.setPower(0.0);
+        motor2.setPower(0.0);
+    }
+
+    // Raise lift to a specific height
+    void raiseLift(double height) {
+        String functionName = "raiseLift";
+        int targetPosition; // In Encoder Ticks
+        int targetPosition2; // In Encoder Ticks
+
+
+        if (DEBUG) {
+            DbgLog.msg("%s", functionName);
+        }
+
+        if (liftState != LiftState.RETRACTED || liftState == LiftState.UNKNOWN) {
+            DbgLog.msg("LiftMechanism is in wrong state.");
+            // Nothing to do
+            return;
+        }
+
+        liftState = LiftState.RAISING;
+
+        // Save the runmode state to restore later
+        DcMotor.RunMode initialRunMode = this.motor1.getMode();
+        DcMotor.RunMode initialRunMode2 = this.motor2.getMode();
+
+        // Set mode to Run to Position
+        this.motor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        this.motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // TODO: Confirm signs below
+        targetPosition = (int)(this.motor1.getCurrentPosition() + height * motorMetadata.get(motor1).encoderCountPerRevolution * LIFT_GEAR_REDUCTION);
+        targetPosition2 = (int)(this.motor2.getCurrentPosition() - height * motorMetadata.get(motor2).encoderCountPerRevolution * LIFT_GEAR_REDUCTION);
+
+        motor1.setTargetPosition(targetPosition);
+        motor2.setTargetPosition(targetPosition2);
+
+        setLiftSpeed(DEFAULT_LIFT_SPEED);
+
+        // Add some logic to detect if lift is stuck moving and cannot finish
+        while (opMode.opModeIsActive() && motor1.isBusy() && motor2.isBusy() && (! liftStuck)) {
+            // Movement happens here...
+            opMode.telemetry.addData("Lift", "Raising lift");
+            opMode.telemetry.update();
+        }
+
+        // Stop motors
+        stopLift();
+        opMode.telemetry.addData("Lift", "Lift raised.");
+        opMode.telemetry.update();
+
+        // Restore mode
+        this.motor1.setMode(initialRunMode);
+        this.motor2.setMode(initialRunMode2);
+
+        liftState = LiftState.RAISED;
+
+        currHeight = height;
+    }
+
+    // Raise lift to the default configured height
+    void raiseLift() {
+        String functionName = "raiseLift";
+
+        if (DEBUG) {
+            DbgLog.msg("%s", functionName);
+        }
+
+        raiseLift(TARGET_HEIGHT_IN);
+    }
+
+    // Retract the lift to starting position
+    void retractLift() {
+        String functionName = "retractLift";
+        int targetPosition; // In Encoder Ticks
+        int targetPosition2; // In Encoder Ticks
+
+        if (DEBUG) {
+            DbgLog.msg("%s", functionName);
+        }
+
+        if (liftState != LiftState.RAISED || liftState != LiftState.UNKNOWN) {
+            DbgLog.msg("LiftMechanism is in wrong state.");
+            // Nothing to do
+            return;
+        }
+
+        liftState = LiftState.LOWERING;
+
+        // Save the runmode state to restore later
+        DcMotor.RunMode initialRunMode = this.motor1.getMode();
+        DcMotor.RunMode initialRunMode2 = this.motor2.getMode();
+
+        // Set mode to Run to Position
+        this.motor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        this.motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // TODO: Confirm signs below
+        targetPosition = (int)(this.motor1.getCurrentPosition() + (-1) * currHeight * motorMetadata.get(motor1).encoderCountPerRevolution * LIFT_GEAR_REDUCTION);
+        targetPosition2 = (int)(this.motor2.getCurrentPosition() - (-1) * currHeight * motorMetadata.get(motor2).encoderCountPerRevolution * LIFT_GEAR_REDUCTION);
+
+        motor1.setTargetPosition(targetPosition);
+        motor2.setTargetPosition(targetPosition2);
+
+        setLiftSpeed(DEFAULT_LIFT_SPEED);
+
+        // Add some logic to detect if lift is stuck moving and cannot finish
+        while (opMode.opModeIsActive() && motor1.isBusy() && motor2.isBusy() && (! liftStuck)) {
+            // Movement happens here...
+            opMode.telemetry.addData("Lift", "Lowering lift");
+            opMode.telemetry.update();
+        }
+        if (liftStuck) {
+            liftState = LiftState.UNKNOWN;
+        }
+
+        // Stop motors
+        stopLift();
+        opMode.telemetry.addData("Lift", "Lift retracted.");
+        opMode.telemetry.update();
+
+        // Restore mode
+        this.motor1.setMode(initialRunMode);
+        this.motor2.setMode(initialRunMode2);
+
+        liftState = LiftState.RETRACTED;
+        currHeight = 0;
+    }
+
+    //
+    // Utility Functions
+    //
+
+    private void opModeRequired() {
+        String functionName = "opModeRequired";
+        String msg = "A LinearOpMode is required";
+
+        if (DEBUG) {
+            DbgLog.msg(msg);
+        }
+        try {
+            throw new Exception(msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void unsupported(String msg) {
+        String functionName = "unsupported";
+
+        if (DEBUG) {
+            DbgLog.msg("%s not supported", functionName);
+        }
+        try {
+            throw new Exception(msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ src/main/java/org/firstinspires/ftc/mentor/LinearSlide.java
@@ -0,0 +1,332 @@
+package org.firstinspires.ftc.mentor;
+
+import com.qualcomm.ftccommon.DbgLog;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import java.util.Map;
+
+class LinearSlide {
+
+    private LinearOpMode opMode = null;
+    private DcMotor slideMotor = null; // One motor with encoder
+    private Map<DcMotor, MotorMetadata> motorMetadata;
+    private boolean DEBUG = false;
+    private double slideCircumference = 1.0;  // Inches
+    private double slideGearReduction = 1.0; // Ratio
+    private double extendDistance = 1.0; // Inches
+    private double speed = 0.5; // Default slide speed
+    private boolean slideStuck = false;
+    private double currentSlidePosition = 0; // Inches
+
+
+    enum SlideState {
+        RETRACTED,
+        EXTENDING,
+        EXTENDED,
+        RETRACTING,
+        UNKNOWN
+    }
+
+    private SlideState slideState = SlideState.RETRACTED;  // Assume slide is in retracted state
+
+    /*
+     * Constructors
+     */
+
+    public LinearSlide(LinearOpMode opMode, DcMotor slideMotor, Map<DcMotor, MotorMetadata> metadata, double extendDistance, double speed, double slideCircumference, boolean isExtended) {
+        this.opMode = opMode;
+        this.slideMotor = slideMotor;
+        this.motorMetadata = metadata;
+        this.extendDistance = extendDistance;
+        this.speed = speed;
+        this.slideCircumference = slideCircumference;
+        if (isExtended) {
+            this.slideState = SlideState.EXTENDED;
+        }
+        else {
+            this.slideState = SlideState.RETRACTED;
+        }
+    }
+
+    // Use this constructor when slide state starts in the extended position
+    public LinearSlide(LinearOpMode opMode, DcMotor slideMotor, Map<DcMotor, MotorMetadata> metadata, boolean isExtended) {
+        this.opMode = opMode;
+        this.slideMotor = slideMotor;
+        this.motorMetadata = metadata;
+        if (isExtended) {
+            this.slideState = SlideState.EXTENDED;
+        }
+        else {
+            this.slideState = SlideState.RETRACTED;
+        }
+    }
+
+    public LinearSlide(LinearOpMode opMode, DcMotor slideMotor, Map<DcMotor, MotorMetadata> metadata) {
+        this.opMode = opMode;
+        this.slideMotor = slideMotor;
+        this.motorMetadata = metadata;
+    }
+
+    /*
+     * Functions
+     */
+
+    SlideState getSlideState() {
+        String functionName = "getSlideState";
+
+        if (DEBUG) {
+            DbgLog.msg("%s", functionName);
+        }
+        return slideState;
+    }
+
+    void setSlideState(SlideState state) {
+        String functionName = "setSlideState";
+
+        if (DEBUG) {
+            DbgLog.msg("%s", functionName);
+        }
+
+        this.slideState = state;
+    }
+
+    double getSpeed() {
+        String functionName = "getSpeed";
+
+        if (DEBUG) {
+            DbgLog.msg("%s", functionName);
+        }
+
+        return speed;
+    }
+
+    void setSpeed(double speed) {
+        String functionName = "setSpeed";
+
+        if (DEBUG) {
+            DbgLog.msg("%s", functionName);
+        }
+
+        this.speed = speed;
+    }
+
+    double getSlideCircumference() {
+        String functionName = "getSlideCircumference";
+
+        if (DEBUG) {
+            DbgLog.msg("%s", functionName);
+        }
+
+        return slideCircumference;
+    }
+
+    void setSlideCircumference(double circumference) {
+        String functionName = "setSlideCircumference";
+
+        if (DEBUG) {
+            DbgLog.msg("%s", functionName);
+        }
+
+        this.slideCircumference = circumference;
+    }
+
+    double getExtendDistance() {
+        String functionName = "getExtendDistance";
+
+        if (DEBUG) {
+            DbgLog.msg("%s", functionName);
+        }
+
+        return extendDistance;
+    }
+
+    void setExtendDistance(double extendDistance) {
+        String functionName = "setExtendDistance";
+
+        if (DEBUG) {
+            DbgLog.msg("%s", functionName);
+        }
+
+        this.extendDistance = extendDistance;
+    }
+
+    // Try to figure out what state the slide is in.
+    // Return the state AND set the internal state to the value discovered.
+    SlideState determineSlideState() {
+        String functionName = "determineSlideState";
+        SlideState state = SlideState.UNKNOWN;
+
+        if (DEBUG) {
+            DbgLog.msg("%s", functionName);
+        }
+
+        unsupported("determineState not implemented yet.");
+
+//        if (SENSOR DETECTS SLIDE IN RETRACTED POSITION) {
+//            state = SlideState.RETRACTED;
+//        }
+//        else {
+//            state = SlideState.EXTENDED;
+//        }
+
+        // if slide motor is moving, then transition state depends on which direction it is moving
+        if (slideMotor.isBusy()) {
+            if (slideMotor.getPower() > 0) {
+                // Extending
+                state = SlideState.EXTENDING;
+            }
+            else {
+                state = SlideState.RETRACTING;
+            }
+
+        }
+
+        // Set slideState to the discovered state
+        slideState = state;
+
+        return slideState;
+    }
+
+    double getSlideGearReduction() {
+        String functionName = "getSlideGearReduction";
+
+        if (DEBUG) {
+            DbgLog.msg("%s", functionName);
+        }
+
+        return slideGearReduction;
+    }
+
+    void setSlideGearReduction(double slideGearReduction) {
+        String functionName = "setSlideGearReduction";
+
+        if (DEBUG) {
+            DbgLog.msg("%s", functionName);
+        }
+
+        this.slideGearReduction = slideGearReduction;
+    }
+
+    double getCurrentSlidePosition() {
+        return currentSlidePosition;
+    }
+
+    void stopSlide() {
+        slideMotor.setPower(0);
+    }
+
+
+    void extendSlide() {
+        String functionName = "extendSlide";
+        int targetPosition;
+
+        if (DEBUG) {
+            DbgLog.msg("%s", functionName);
+        }
+
+        if (slideState != SlideState.RETRACTED || slideState == SlideState.UNKNOWN) {
+            DbgLog.msg("LinearSlide is in wrong state");
+
+            return;
+        }
+
+        slideState = SlideState.EXTENDING;
+
+        // Move Slide
+        // Save the runmode state to restore later
+        DcMotor.RunMode initialRunMode = this.slideMotor.getMode();
+
+        // Set mode to Run to Position
+        this.slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // TODO: Confirm sign below
+        targetPosition = (int)(this.slideMotor.getCurrentPosition() + extendDistance * motorMetadata.get(slideMotor).encoderCountPerRevolution * slideGearReduction);
+
+        slideMotor.setTargetPosition(targetPosition);
+
+        slideMotor.setPower(speed);
+
+        // Add some logic to detect if lift is stuck moving and cannot finish
+        while (opMode.opModeIsActive() && slideMotor.isBusy() && (! slideStuck)) {
+            // Movement happens here...
+            opMode.telemetry.addData("LinearSlide", "Extending slide...");
+            opMode.telemetry.update();
+        }
+
+        // Stop motors
+        stopSlide();
+        opMode.telemetry.addData("LinearSlide", "Slide extended.");
+        opMode.telemetry.update();
+
+        // Restore mode
+        this.slideMotor.setMode(initialRunMode);
+
+        slideState = SlideState.EXTENDED;
+        currentSlidePosition = extendDistance;
+    }
+
+    void retractSlide() {
+        String functionName = "retractSlide";
+        int targetPosition;
+
+        if (DEBUG) {
+            DbgLog.msg("%s", functionName);
+        }
+
+        if (slideState != SlideState.EXTENDED || slideState == SlideState.UNKNOWN) {
+            DbgLog.msg("LinearSlide is in wrong state");
+            return;
+        }
+
+        slideState = SlideState.RETRACTING;
+
+        // Save the runmode state to restore later
+        DcMotor.RunMode initialRunMode = this.slideMotor.getMode();
+
+        // Set mode to Run to Position
+        this.slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // TODO: Confirm sign below
+        targetPosition = (int)(this.slideMotor.getCurrentPosition() - currentSlidePosition * motorMetadata.get(slideMotor).encoderCountPerRevolution * slideGearReduction);
+
+        slideMotor.setTargetPosition(targetPosition);
+
+        slideMotor.setPower(speed);
+
+        // Add some logic to detect if lift is stuck moving and cannot finish
+        while (opMode.opModeIsActive() && slideMotor.isBusy() && (! slideStuck)) {
+            // Movement happens here...
+            opMode.telemetry.addData("LinearSlide", "Retracting slide");
+            opMode.telemetry.update();
+        }
+
+        // Stop motors
+        stopSlide();
+        opMode.telemetry.addData("LinearSlide", "Slide retracted.");
+        opMode.telemetry.update();
+
+        // Restore mode
+        this.slideMotor.setMode(initialRunMode);
+
+        slideState = SlideState.RETRACTED;
+    }
+
+    /*
+     * Utilities
+     */
+
+    private void unsupported(String msg) {
+        String functionName = "unsupported";
+
+        if (DEBUG) {
+            DbgLog.msg("%s not supported", functionName);
+        }
+        try {
+            throw new Exception(msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
