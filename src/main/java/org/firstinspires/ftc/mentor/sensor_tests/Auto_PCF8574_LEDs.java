2d06f854a0189f3592002449f2c6adff693f3b4b
Mark Dolecki <markdolecki@yahoo.com>
2017-02-18 22:30:20 -0800

    New sensor class and test autonomous for the PCF8574P i2c expander.  This class allows the independent control of eight LEDs connected to the PCF8574P.  Also can be used to control a seven segment display.

---
+++ src/main/java/org/firstinspires/ftc/mentor/sensor_tests/Auto_PCF8574_LEDs.java
@@ -0,0 +1,115 @@
+/*
+Copyright (c) 2016 Robert Atkinson
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Robert Atkinson nor the names of his contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.firstinspires.ftc.mentor.sensor_tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.mentor.sensors.PCF8574_LEDs;
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="Auto_PCF8574_LEDs", group="SensorTests")  // @Autonomous(...) is the other common choice
+//@Disabled
+public class Auto_PCF8574_LEDs extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+        I2cDeviceSynch deviceSynch = hardwareMap.i2cDeviceSynch.get("pcf");
+        PCF8574_LEDs pcf = new PCF8574_LEDs(deviceSynch, true);
+
+        pcf.allOn();
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        /* eg: Initialize the hardware variables. Note that the strings used here as parameters
+         * to 'get' must correspond to the names assigned during the robot configuration
+         * step (using the FTC Robot Controller app on the phone).
+         */
+        // leftMotor  = hardwareMap.dcMotor.get("left_drive");
+        // rightMotor = hardwareMap.dcMotor.get("right_drive");
+
+        // eg: Set the drive motor directions:
+        // "Reverse" the motor that runs backwards when connected directly to the battery
+        // leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        // rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+
+        // Wait for the game to start (driver presses PLAY)
+        // waitForStart();
+        while (! isStarted()) {
+            // Wait for start
+            telemetry.addData("Press start", ">>>");
+            telemetry.update();
+        }
+
+        runtime.reset();
+
+        pcf.allOff();
+
+        int counter = 0;
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+
+            pcf.setLEDs(counter);
+
+            telemetry.update();
+            counter++;
+            if (counter > 0xff) {
+                counter = 0;
+            }
+        }
+        pcf.allOff();
+    }
+}
---
+++ src/main/java/org/firstinspires/ftc/mentor/sensors/PCF8574_LEDs.java
@@ -0,0 +1,176 @@
+package org.firstinspires.ftc.mentor.sensors;
+
+import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchDevice;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by markdolecki on 1/28/17.
+ *
+ * This class can be used to control up to eight individual LEDs or a seven segment numeric display.
+ */
+
+public class PCF8574_LEDs extends I2cDeviceSynchDevice<I2cDeviceSynch> {
+
+    private static String DEVICE_NAME = "PCF8574";
+
+    private boolean DEBUG = false;
+
+    protected static final int MUX_I2C_ADDRESS = 0x20; // Address for PCF8574 is 0x40
+
+    public final static I2cAddr ADDRESS_I2C_DEFAULT = I2cAddr.create7bit(MUX_I2C_ADDRESS);
+
+    private int currentState = 0;
+
+    // Data pins on the PCF8574P
+    public int P0 = (1 << 0);
+    public int P1 = (1 << 1);
+    public int P2 = (1 << 2);
+    public int P3 = (1 << 3);
+    public int P4 = (1 << 4);
+    public int P5 = (1 << 5);
+    public int P6 = (1 << 6);
+    public int P7 = (1 << 7);
+
+    // Bit masks for 7 segment LED display
+    //    P0
+    //  P5  P1
+    //    P6
+    //  P4  P2
+    //    P3
+    // Note: P7 is the dot
+    private int Zero = P0 | P1 | P2 | P3 | P4 | P5;
+    private int One = P1 | P2;
+    private int Two = P0 | P1 | P3 | P4 | P6;
+    private int Three = P0 | P1 | P2 | P3 | P6;
+    private int Four = P1 | P2 | P5 | P6;
+    private int Five = P0 | P2 | P3 | P5 | P6;
+    private int Six = P0 | P2 | P3 | P4 | P5 | P6;
+    private int Seven = P0 | P1 | P2;
+    private int Eight = P0 | P1 | P2 | P3 | P4 | P5 | P6;
+    private int Nine = P0 | P1 | P2 | P3 | P5 | P6;
+    private int Dot = P7;
+
+
+    // Assumes eight (8) LEDs are connected to the PCF8574P chip
+
+    public PCF8574_LEDs(I2cDeviceSynch deviceClient, boolean isOwned) {
+        super(deviceClient, isOwned);
+
+        this.deviceClient.setI2cAddress(ADDRESS_I2C_DEFAULT);
+        this.deviceClient.engage();
+
+        super.registerArmingStateCallback();
+    }
+
+
+    /**
+     * Actually carries out the initialization of the instance.
+     *
+     * @return Whether the initialization was successful or not
+     */
+    @Override
+    protected boolean doInitialize() {
+        // Nothing to do here
+        return true;
+    }
+
+    /**
+     * Returns an indication of the manufacturer of this device.
+     *
+     * @return the device's manufacturer
+     */
+    @Override
+    public Manufacturer getManufacturer() {
+        return Manufacturer.Other;
+    }
+
+    /**
+     * Returns a string suitable for display to the user as to the type of device
+     *
+     * @return device manufacturer and name
+     */
+    @Override
+    public String getDeviceName() {
+        return DEVICE_NAME;
+    }
+
+    //
+    // Functions
+    //
+
+    public void allOn() {
+        writeData(0xff);
+    }
+
+    public void allOff() {
+        writeData(0x00);
+    }
+
+    public int getCurrentState() {
+        return currentState;
+    }
+
+    public void setLEDs(int mask) {
+        int leds = Range.clip(mask, 0, 0xff);
+        writeData(leds);
+    }
+
+    public void showDigit(int digit) {
+        digit = Range.clip(digit,0,9);
+        switch (digit) {
+            case 0:
+                writeData(Zero);
+                break;
+            case 1:
+                writeData(One);
+                break;
+            case 2:
+                writeData(Two);
+                break;
+            case 3:
+                writeData(Three);
+                break;
+            case 4:
+                writeData(Four);
+                break;
+            case 5:
+                writeData(Five);
+                break;
+            case 6:
+                writeData(Six);
+                break;
+            case 7:
+                writeData(Seven);
+                break;
+            case 8:
+                writeData(Eight);
+                break;
+            case 9:
+                writeData(Nine);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void showDot(boolean show) {
+        if (show) {
+            writeData(Dot);
+        }
+        else {
+            writeData(~Dot);
+        }
+    }
+
+    //
+    // Utility
+    //
+
+    private void writeData(int data) {
+        int output = ~data;
+
+        this.deviceClient.write8(0x00, output);
+    }
+}
